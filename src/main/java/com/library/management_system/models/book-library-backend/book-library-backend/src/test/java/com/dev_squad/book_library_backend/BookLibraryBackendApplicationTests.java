fa5640dc808eb9929fc3cce3c4026f63ce978d82
Abanda Glory <abandaglory91@gmail.com>
2025-09-03 11:19:29 +0100

    initial commit

---
+++ src/main/java/com/library/management_system/models/book-library-backend/book-library-backend/src/test/java/com/dev_squad/book_library_backend/BookLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_squad.book_library_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLibraryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
